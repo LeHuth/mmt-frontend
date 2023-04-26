7450a8df7086367d6bb89c33af92784248027184
Daniel Muehlbayer 914019 <s84963@bht-berlin.de>
2023-04-26 17:20:28 +0200

    test 1

---
+++ components/mybutton.vue
@@ -0,0 +1,11 @@
+<template>
+    <div v-if="">
+        {{ myVarText }}
+    </div>
+</template>
+
+<!--ts-code-->
+<script lang="ts">
+const myVarText = 'myVarText';
+</script>
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+// pages/login.vue
+<template>
+    <div class="container">
+        <h1>Anmelden</h1>
+        <form>
+            <div>
+                <label for="email">E-Mail-Adresse:</label>
+                <input type="email" id="email" required />
+            </div>
+            <div>
+                <label for="password">Passwort:</label>
+                <input type="password" id="password" required />
+            </div>
+            <button type="submit">Anmelden</button>
+        </form>
+    </div>
+</template>
+
+<script lang="ts"></script>
+
+<style scoped>
+/* Add your styles here */
+</style>
