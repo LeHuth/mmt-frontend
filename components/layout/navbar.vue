66dd143a2b26ef5220338c8d674a92fc1691b5d6
Daniel Muehlbayer 914019 <s84963@bht-berlin.de>
2023-05-07 20:22:38 +0200

    Logoänderung, Anpassung der Seiten

--- components/layout/navbar.vue
+++ components/layout/navbar.vue
@@ -67,7 +67,7 @@ watch(
                             <span class="sr-only">Home</span>
                             <img
                                 class="h-16 md:h-24 w-auto transition duration-500 scale-50 md:hover:scale-110"
-                                src="/img/MMTLogoSmall.png"
+                                src="/img/LogoMMT.png"
                             />
                         </nuxt-link>
                     </div>
