d36e602956f3c3e40e816b8984dd924f9bd39793
DanielMuehlbayer <daniel.muehlbayer@gmail.com>
2023-05-15 05:59:48 +0200

    Home/Login/Register-Pages

--- components/layout/navbar.vue
+++ components/layout/navbar.vue
@@ -76,14 +76,14 @@ watch(
                     class="hidden md:flex justify-between space-x-10 align-bottom mt-14"
                 >
                     <div class="hidden md:flex space-x-10 align-bottom">
-                        <nuxt-link to="/articles/overview">
+                        <nuxt-link to="/tickets">
                             <span
                                 class="text-base font-medium text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-300"
                             >
                                 TICKETS
                             </span>
                         </nuxt-link>
-                        <nuxt-link to="/topics">
+                        <nuxt-link to="/karte">
                             <span
                                 class="text-base font-medium text-gray-500 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-300"
                             >
@@ -310,81 +310,11 @@ watch(
                             <span class="ml-3">News</span>
                         </NuxtLink>
                     </li>
+                   
                     <li>
-                        <NuxtLink
-                            to="/subscribe"
-                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                        >
-                            <svg
-                                aria-hidden="true"
-                                class="flex-shrink-0 w-6 h-6 text-gray-500 dark:text-gray-200 transition duration-75 group-hover:text-gray-900 dark:hover:text-gray-300 dark:group-hover:text-white"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-                                ></path>
-                            </svg>
-                            <span class="flex-1 ml-3 whitespace-nowrap"
-                                >Tickets</span
-                            >
-                            <span
-                                class="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300"
-                                >Pro</span
-                            >
-                        </NuxtLink>
+         
                     </li>
-                    <li>
-                        <NuxtLink
-                            to="/topics"
-                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-6 h-6"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
-                                />
-                            </svg>
 
-                            <span class="flex-1 ml-3 whitespace-nowrap"
-                                >MAP</span
-                            >
-                            <!-- <span class="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">3</span> -->
-                        </NuxtLink>
-                    </li>
-                    <li>
-                        <NuxtLink
-                            to="/tutorials"
-                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke-width="1.5"
-                                stroke="currentColor"
-                                class="w-6 h-6"
-                            >
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
-                                />
-                            </svg>
-
-                            <span class="flex-1 ml-3 whitespace-nowrap"
-                                >Tutorials</span
-                            >
-                        </NuxtLink>
-                    </li>
                     <li>
                         <NuxtLink
                             to="/articles"
