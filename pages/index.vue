f578b48d381e005cf3f24e4934c4a5b0f1a41640
Leo <s80387@beuth-hochschule.de>
2023-06-26 02:19:56 +0200

    deploy fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,28 @@
 <template>
-    <div>
-        <client-only>
-            <MMTMap/>
-            <SeSection/>
-        </client-only>
-
-    </div>
+  <div>
+    <client-only>
+      <MMTMap />
+    </client-only>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import {useAuthStore} from '~/store/auth';
-import '@maptiler/sdk/dist/maptiler-sdk.css';
-import {useEventsStore} from '~/store/events';
-import MMTMap from '~/components/MMT-Map.vue';
+import { useAuthStore } from '~/store/auth'
+import '@maptiler/sdk/dist/maptiler-sdk.css'
+import { useEventsStore } from '~/store/events'
+import MMTMap from '~/components/MMT-Map.vue'
 
 definePageMeta({
-    title: 'Home',
-    pageTransition: {
-        name: 'slide-right',
-        mode: 'default'
-    },
-});
+  title: 'Home',
+  pageTransition: {
+    name: 'slide-right',
+    mode: 'default'
+  }
+})
+const authStore = useAuthStore()
+const eventStore = useEventsStore()
+await eventStore.fetchEvents()
 
-const authStore = useAuthStore();
-const eventStore = useEventsStore();
-await eventStore.fetchEvents();
-
-
-const previewImage = ref<File | null>(null);
-const raw = ref<File | null>(null);
-const liveURL = ref<string | null>(null);
-
-const eventObj = {
-    title: 'Brutalismus 3000',
-    description: 'Nu gabber post techno punk from berlin with love',
-    date: new Date(),
-    time: '23:00',
-    location: 'Test Location',
-    category: 'TestCategory',
-    tags: ['tag1', 'tag2'],
-    organizer: '645f6ddf2fc82cebeb3cedd9',
-    image: null,
-    imageName: null,
-    ticketInfo: {
-        ticketTypes: [
-            {
-                name: 'General Admission',
-            },
-        ],
-        name: 'General Admission',
-        price: 20,
-        available: 100,
-    },
-};
-
-const login = () => {
-    authStore
-        .login('admin@mail.de', 'admin')
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-};
 </script>
 
 <style scoped>
