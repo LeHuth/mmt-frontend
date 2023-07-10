79b7d81a3abace995ca107b3224d859d385954d6
Leo <s80387@beuth-hochschule.de>
2023-07-10 10:45:34 +0200

    map preveiwe

--- components/MMT-Map.vue
+++ components/MMT-Map.vue
@@ -1,55 +1,46 @@
 <template>
-    <div>
-        <div class="drawer drawer-end">
-            <input id="my-drawer-4" class="drawer-toggle" type="checkbox"/>
-            <div class="drawer-content">
-                <div class="map-buttons">
-                    <div id="map-btn">
-                        <div id="drawbtn1">
-                            <label @click="setEvent(events[0])" class="drawer-button btn btn-primary" for="my-drawer-4">{{events[0].title}}</label>
-                        </div>
-                    </div>
-                    <div id="map1-btn">
-                        <div id="drawbtn">
-                            <label @click="setEvent(events[1])" class="drawer-button btn btn-primary" for="my-drawer-4">{{ events[1].title }}</label>
-                        </div>
-                    </div>
-                    <div id="map2-btn">
-                        <div id="drawbtn2">
-                            <label @click="setEvent(events[2])" class="drawer-button btn btn-primary" for="my-drawer-4">{{events[2].title}}</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="map-and-popup">
-                    <div id="map" ref="map"></div>
-                </div>
-
-
-            </div>
-            <div class="drawer-side">
-                <label class="drawer-overlay" for="my-drawer-4"></label>
-                <ul class="menu p-4 w-80  bg-base-200 text-base-content">
-                    <Popup
-                            v-if="true"
-                            :show="true"
-                            :description="selectedEvent.title"
-                            :image="selectedEvent.image == 'test-image-url' ? 'https://picsum.photos/200' : selectedEvent.image"
-                            imageAlt="Popup Image"
-                            :event_id="selectedEvent._id"
-                    />
-                </ul>
-            </div>
+  <div class="max-w-[1475px] ml-auto mr-auto my-6">
+    <div class="map-buttons">
+      <div id="map-btn">
+        <div id="drawbtn1">
+          <label class="drawer-button btn btn-primary" for="my-drawer-4" @click="setEvent(events[0])">{{
+            events[0].title
+          }}</label>
         </div>
+      </div>
+      <div id="map1-btn">
+        <div id="drawbtn">
+          <label class="drawer-button btn btn-primary" for="my-drawer-4" @click="setEvent(events[1])">{{
+            events[1].title
+          }}</label>
+        </div>
+      </div>
+      <div id="map2-btn">
+        <div id="drawbtn2">
+          <label class="drawer-button btn btn-primary" for="my-drawer-4" @click="setEvent(events[2])">{{
+            events[2].title
+          }}</label>
+        </div>
+      </div>
     </div>
+    <div class="grid gap-3" style="grid-template-columns: 980px 480px">
+      <div class="map-and-popup max-w-[980px] border border-black">
+        <div id="map" ref="map" />
+      </div>
+      <div id="event-scoll" class="max-w-[480px] overflow-y-scroll">
+        <Card v-for="event in events" :event-data="event" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import {defineComponent, onMounted, ref} from 'vue';
-import Popup from '~/components/Popup.vue';
-import {config, Map, MapOptions, Marker} from '@maptiler/sdk';
-import EventMapCard from '~/components/EventMapCard.vue';
-import '@maptiler/sdk/dist/maptiler-sdk.css';
-import {useEventsStore} from "~/store/events";
+import { defineComponent, onMounted, ref } from 'vue'
+import { config, Map, MapOptions, Marker } from '@maptiler/sdk'
+import EventMapCard from '~/components/EventMapCard.vue'
+import '@maptiler/sdk/dist/maptiler-sdk.css'
+import { useEventsStore } from '~/store/events'
+
 const eventStore = useEventsStore()
 eventStore.fetchEvents()
 const events = eventStore.getEvents
@@ -57,24 +48,24 @@ const events = eventStore.getEvents
 const selectedEvent = ref({})
 
 defineComponent({
-    name: 'MMT-Map',
-    components: {
-        EventMapCard,
-    },
-});
+  name: 'MMTMap',
+  components: {
+    EventMapCard
+  }
+})
 
 const setEvent = (event: any) => {
-    selectedEvent.value = event
-    console.log(selectedEvent.value)
+  selectedEvent.value = event
+  console.log(selectedEvent.value)
 }
 
-const showPopup = ref(false);
-const showPopup1 = ref(false);
+const showPopup = ref(false)
+const showPopup1 = ref(false)
 
-const mapZoom = ref(10);
-let mapOptions: MapOptions;
-let myMap: Map;
-let marker: Marker;
+const mapZoom = ref(10)
+let mapOptions: MapOptions
+let myMap: Map
+let marker: Marker
 /*
 const logZoom = () => {
     //console.log(Math.floor(myMap.getZoom()))
@@ -91,52 +82,52 @@ const readZoom = computed(() => {
 })
 */
 onMounted(() => {
-    let myMap: Map;
-    config.apiKey = 'tQT7W72zRJXId5YzduvP';
-    let options: MapOptions = {
-        container: document.getElementById('map') as HTMLElement, // container's id or the HTML element in which SDK will render the map
-        style: 'https://api.maptiler.com/maps/16709723-1898-467b-bc2b-083dbe94dcfa/style.json?key=luHiVJHYFAkokuoL2QwT',
-        center: [13.4, 52.5],
-        pitch: 85,
-        zoom: 12,
-    };
-    myMap = new Map(options);
-    //myMap.on('zoom', logZoom)
-    const marker3 = new Marker({
-        color: '#fc1414',
-        draggable: false,
-        element: document.getElementById('drawbtn'),
-        scale: 2,
-    })
-        .setLngLat([13.4, 52.6])
-        .addTo(myMap);
+  let myMap: Map
+  config.apiKey = 'tQT7W72zRJXId5YzduvP'
+  const options: MapOptions = {
+    container: document.getElementById('map') as HTMLElement, // container's id or the HTML element in which SDK will render the map
+    style: 'https://api.maptiler.com/maps/backdrop/style.json?key=tQT7W72zRJXId5YzduvP',
+    center: [13.4, 52.5],
+    pitch: 85,
+    zoom: 12
+  }
+  myMap = new Map(options)
+  // myMap.on('zoom', logZoom)
+  const marker3 = new Marker({
+    color: '#fc1414',
+    draggable: false,
+    element: document.getElementById('drawbtn'),
+    scale: 2
+  })
+    .setLngLat([13.4, 52.6])
+    .addTo(myMap)
 
-    const marker4 = new Marker({
-        color: '#fc1414',
-        draggable: false,
-        element: document.getElementById('drawbtn1'),
-        scale: 2,
-    })
-        .setLngLat([13.3, 52.4])
-        .addTo(myMap);
+  const marker4 = new Marker({
+    color: '#fc1414',
+    draggable: false,
+    element: document.getElementById('drawbtn1'),
+    scale: 2
+  })
+    .setLngLat([13.3, 52.4])
+    .addTo(myMap)
 
-    const marker5 = new Marker({
-        color: '#fc1414',
-        draggable: false,
-        element: document.getElementById('drawbtn2'),
-        scale: 2,
-    })
-        .setLngLat([13.4, 52.5])
-        .addTo(myMap);
+  const marker5 = new Marker({
+    color: '#fc1414',
+    draggable: false,
+    element: document.getElementById('drawbtn2'),
+    scale: 2
+  })
+    .setLngLat([13.4, 52.5])
+    .addTo(myMap)
 
-    const marker2 = new Marker({
-        color: '#fc1414',
-        draggable: false,
-        element: document.getElementById('map-tip'),
-    })
-        .setLngLat([13.3, 52.5])
-        .addTo(myMap);
-});
+  const marker2 = new Marker({
+    color: '#fc1414',
+    draggable: false,
+    element: document.getElementById('map-tip')
+  })
+    .setLngLat([13.3, 52.5])
+    .addTo(myMap)
+})
 </script>
 
 <style scoped>
@@ -145,64 +136,16 @@ onMounted(() => {
     box-shadow: 0 2px 4px rgb(255, 255, 255);
 }
 
-.drawer, .drawer-content{
+.drawer, .drawer-content {
     max-height: calc(100vh - 240px);;
 }
 
-.menu{
+.menu {
     background-color: white;
 }
 
-#map {
+#map, #event-scoll {
     flex: 2;
     height: calc(100vh - 240px);
-    box-shadow: 0 2px 4px rgba(255, 255, 255, 0.2);
-    background-color: #21D4FD;
-    background-image: linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);
-    border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
-    box-shadow: 15px 15px 50px rgba(0, 0, 0, 0.2);
-    animation: morphing 10s infinite;
-}
-
-.btn {
-    display: inline-block;
-    margin-bottom: 10px;
-    border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
-    padding: 10px;
-    cursor: pointer;
-    animation: morphing 10s infinite;
-}
-
-.btn:hover {
-    animation-play-state: paused;
-}
-
-.btn-e1 {
-    background-color: #ff01f2;
-}
-
-.btn-e2 {
-    background-color: #00aaff;
-}
-
-.btn-e3 {
-    background-color: #ff7700;
-}
-
-@keyframes morphing {
-    0% {
-        border-radius: 30% 70% 70% 30% / 30% 30% 70% 70%;
-        box-shadow: 15px 15px 50px rgba(0, 0, 0, 0.2);
-    }
-    25% {
-        border-radius: 58% 42% 75% 25% / 76% 46% 54% 24%;
-    }
-    50% {
-        border-radius: 50% 50% 33% 67% / 55% 27% 73% 45%;
-        box-shadow: -10px -5px 50px rgba(0, 0, 0, 0.2);
-    }
-    75% {
-        border-radius: 33% 67% 58% 42% / 63% 68% 32% 37%;
-    }
 }
 </style>
